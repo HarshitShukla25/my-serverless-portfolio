977315ae9af9daaddcb7260f08b9f51001ec74c9
Harshit <harshitshukla.eee18@itbhu.ac.in>
2020-10-07 21:16:38 +0530

    Major Changes

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,56 +1,18 @@
-import StringIO
-import zipfile
-import mimetypes
-
 import boto3
 from botocore.client import Config
+import StringIO
+import zipfile
 
+s3 = boto3.resource('s3',config=Config(signature_version='s3v4'))
 
+portfolio_bucket = s3.Bucket('portfolio.harshitshukla.live')
+build_bucket = s3.Bucket('portfoliobuild.harshitshukla.live')
 
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
 
-def lambda_handler(event, context):
-    sns = boto3.resource('sns')
-    topic = sns.Topic('arn:aws:sns:us-east-2:394994090208:deployPortfolioTopic')
-    
-    location = {
-        "bucketName": 'portfoliobuild.blualism.com',
-        "objectKey": 'portfoliobuild.zip'
-    }
-    
-    job = event.get('CodePipeline.job')
-    if job:
-        for artifact in job["data"]["inputArtifacts"]:
-            if artifact["name"] == "MyAppBuild":
-                location = artifact["location"]["s3Location"]
-    
-    print "Building portfolio from " + str(location)
-    
-    
-    
-    try:
-        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-        
-        portfolio_bucket = s3.Bucket('portfolio.blualism.com')
-        build_bucket = s3.Bucket(location["bucketName"])
-        
-        portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
-        
-        with zipfile.ZipFile(portfolio_zip) as myzip:
-            for nm in myzip.namelist():
-                obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-        
-        
-        print "Job done"
-        topic.publish(Subject='Portfolio Deployed', Message="Portfolio deployed successfully.")
-        if job:
-            codepipeline = boto3.client('codepipeline')
-            codepipeline.put_job_success_result(jobId=job["id"])
-    except:
-        topic.publish(Subject='Portfolio Deployed Failed', Message="The portfolio was not deployed successfully.")
-        raise
-    
-    return "Hello from lambda"
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj,nm)
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
