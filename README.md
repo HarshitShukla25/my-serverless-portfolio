a38e0cab708b15beba6f1658ebba9035a6014273
Harshit <harshitshukla.eee18@itbhu.ac.in>
2020-10-07 11:59:05 +0530

    Added the resume template

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
+# Michael Bianchi's portfolio
+Professional portfolio prototype using react on AWS serverless platform.
 
-open source resume(html+js+css)
-
-<h2>开源简历（open source resume）</h2>
-自己应聘做了一个简历,喜欢的可以去改成自己；
-code写的也不够标准，有兴趣的可以协助修改下。
-
-一些特色：
-<ul>
-  <li>响应式布局</li>
-  <li>局部打印</li>
-  <li>语音操作</li>
-</ul>
-
-
+## Technologies Used
+Git
+SSH
+HTML
+CSS
---
+++ js/main.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
+
+
+const myWork = [
+    {
+        'title': "Work Example",
+        'href': "https://example.com",
+        'desc': "lorem ipsum dolor sit amet",
+        'image': {
+            'desc': "example screenshot of a project involving code",
+            'src': "images/example1.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "Work Example",
+        'href': "https://example.com",
+        'desc': "lorem ipsum dolor sit amet",
+        'image': {
+            'desc': "example screenshot of a project involving chemistry",
+            'src': "images/example2.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "Work Example",
+        'href': "https://example.com",
+        'desc': "lorem ipsum dolor sit amet",
+        'image': {
+            'desc': "example screenshot of a project involving cats",
+            'src': "images/example3.png",
+            'comment': ""
+        }
+    }
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
